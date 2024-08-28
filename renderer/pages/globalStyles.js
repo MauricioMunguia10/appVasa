73debc766d36a4e2423ffdf75fdde0dd155ba534
Mauricio Munguia Garcia <mauriciomunguiagarcia@gmail.com>
2024-08-28 10:23:43 -0600

    logIn signIn

---
+++ renderer/pages/globalStyles.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size:18px;
+  --alpha: #E71D36;
+  --beta: #EA2E33;
+  --gamma: #ED3E30;
+  --delta:#F35E29;
+  --epsilon:#FF9F1C;
+  --psi:#FECF8C;
+  --omega: #FDFFFC;
+
+}
+  body,  html, #__next  {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  
+    
+  }
+      * {
+    box-sizing: border-box;
+  }
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: var(--terciario) var(--alpha);
+  }
+    /* Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 17px;
+}
+
+*::-webkit-scrollbar-track {
+  background: var(--terciario);
+  border-radius: 5px;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: var(--alpha);
+  border-radius: 14px;
+  border: 3px solid var(--terciario);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: var(--alpha);
+}
+`;
+
+export default GlobalStyle;
--- renderer/pages/signIn.jsx
+++ renderer/pages/signIn.jsx
@@ -1,26 +1,26 @@
 import Head from "next/head";
-import styles from "../styles/home.module.css";
+import styles from "../styles/signIn.module.css";
 import GlobalStyle from "./globalStyles";
 import { Fragment } from "react";
 import { useRouter } from "next/navigation";
 
-export default function HomePage() {
+const SignIn = () => {
   const router = useRouter();
   const handleLogIn = () => {
-    router.push("/dashboard");
+    router.push("/home");
   };
   const handleSignIn = () => {
-    router.push("/signIn");
+    router.push("/dashboard");
   };
   return (
     <Fragment>
       <GlobalStyle />
       <Head>
-        <title>Log In | MMG</title>
+        <title>Sign In | MMG</title>
       </Head>
       <div className={styles.container}>
         <div className={styles.card}>
-          <p className={styles.titulo}>Iniciar Sesión</p>
+          <p className={styles.titulo}>Registrarse</p>
           <label htmlFor="email">Correo electrónico</label>
           <input
             id="email"
@@ -36,15 +36,24 @@ export default function HomePage() {
             placeholder="Contraseña"
             className={styles.input}
           />
+          <label htmlFor="password">Repita contraseña</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Contraseña"
+            className={styles.input}
+          />
 
-          <button className={styles.logInButton} onClick={handleLogIn}>
-            Iniciar Sesión
-          </button>
           <button className={styles.signInButton} onClick={handleSignIn}>
             Registrarse
+          </button>
+          <p>¿Ya tiene cuenta?</p>
+          <button className={styles.logInButton} onClick={handleLogIn}>
+            Iniciar Sesión
           </button>
         </div>
       </div>
     </Fragment>
   );
-}
+};
+export default SignIn;
